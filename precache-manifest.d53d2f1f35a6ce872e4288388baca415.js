1daa2855140c2fde945a70f002b07e9c17bff6c1
cyrus.tang <cyrus.tang@revolut.com>
2018-11-19 18:39:25 +0000

    Updates

---
+++ precache-manifest.d53d2f1f35a6ce872e4288388baca415.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d9fad3d88090b8226ac",
+    "url": "/static/css/main.d7bbfb3f.chunk.css"
+  },
+  {
+    "revision": "0d9fad3d88090b8226ac",
+    "url": "/static/js/main.0d9fad3d.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "32f01abd61d5e8f6cb0a6f3e52d2efb6",
+    "url": "/index.html"
+  }
+];
